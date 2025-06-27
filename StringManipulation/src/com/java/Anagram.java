8d4ee8757b816f9f28d34480ee7db2750f61ba4f
sayalikurhe66 <sayalikurhe66@gmail.com>
2025-06-28 03:46:20 +0530

    Anagram

---
+++ StringManipulation/src/com/java/Anagram.java
@@ -0,0 +1,39 @@
+package com.java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+
+    public static boolean isAnagram(String str1, String str2) {
+       
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Second string: ");
+        String str2 = scanner.nextLine();
+
+        if (isAnagram(str1, str2)) {
+            System.out.println("Output: true");
+        } else {
+            System.out.println("Output: false");
+        }
+        
+    }
+}
