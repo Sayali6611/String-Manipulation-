0f08635d7adb9899f91eca22d25107fb31d41935
Sayali Kurhe <sayalikurhe66@gmail.com>
2025-07-11 15:39:28 +0530

    Update Anagram.java

--- StringManipulation/src/com/java/Anagram.java
+++ StringManipulation/src/com/java/Anagram.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 public class Anagram {
 
     public static boolean isAnagram(String str1, String str2) {
-       
+
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+        
         if (str1.length() != str2.length()) {
             return false;
         }
